a5788df748da5ca317e32553ffbcbf083093841e
Postali Web Development <181128403+Postali-Webdev@users.noreply.github.com>
2026-03-24 16:21:37 -0400

    fixing name

--- knr-legal/archive-directors.php
+++ knr-legal/archive-directors.php
@@ -7,7 +7,20 @@
  */
 
 
-get_header(); ?>
+get_header(); 
+
+
+$wp_query = new WP_Query( array(
+    'posts_per_page'    => -1,
+    'post_type'         => 'Directors', 
+    'order'             => 'ASC',
+    'meta_key'			=> 'last_name',
+	'orderby'			=> 'meta_value',
+    'post_status'       => 'publish',
+) );
+
+
+?>
 
 <div class="body-container">    
 
@@ -16,7 +29,7 @@ get_header(); ?>
             <div class="columns">
                 <?php if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<p id="breadcrumbs">','</p>');} ?> 
                 <div class="column-full block">
-                    <h1>Our Directors</h1>
+                    <h1>Our Leadership</h1>
                 </div>
             </div>
         </div>
@@ -25,7 +38,7 @@ get_header(); ?>
     <section class="all-attorneys">
         <div class="container">
             <div class="columns">
-            <?php while( have_posts() ) : the_post(); ?>
+            <?php while( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
                 <div class="column-25 block">
                     <div class="attorney-profile-box">
                         <div class="attorney-photo">
